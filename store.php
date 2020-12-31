cb0557e7b43b70de144db19b26131ad054350268
Safarov-E <elkhan.safarov10@yandex.com>
2020-12-31 21:22:00 +0500

    создание функции добавление задачи

--- store.php
+++ store.php
@@ -1,9 +1,12 @@
 <?php
-    $pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
-    $statement = $pdo->prepare("INSERT INTO tasks (title, content) VALUES (:title, :content)");
-    $statement->bindParam(":title", $_POST['title']);
-    $statement->bindParam(":content", $_POST['content']);
-    $statement->execute();
+    function addTask() {
+        $pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
+        $statement = $pdo->prepare("INSERT INTO tasks (title, content) VALUES (:title, :content)");
+        $statement->bindParam(":title", $_POST['title']);
+        $statement->bindParam(":content", $_POST['content']);
+        $statement->execute();
 
-    header("Location: /"); exit;
+        header("Location: /"); exit;
+    }
+    addTask();
 ?>
