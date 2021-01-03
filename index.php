1ce9eaf4caf147f497914fb036dcf8344832b543
Safarov-E <elkhan.safarov10@yandex.com>
2021-01-03 19:38:43 +0500

    создание класса QueryBuilder

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-    function getAllTasks() {
-        $pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
-        $statement = $pdo->prepare("SELECT * FROM  tasks");
-        $statement->execute();
-        return $statement->fetchAll(2);
+    class QueryBuilder {
+        function getAllTasks() {
+            $pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
+            $statement = $pdo->prepare("SELECT * FROM  tasks");
+            $statement->execute();
+            return $statement->fetchAll(2);
+        }
     }
-    $tasks = getAllTasks();
+    $db = new QueryBuilder;
+    $tasks = $db->getAllTasks();
 ?>
 <!DOCTYPE html>
 <html lang="en">
