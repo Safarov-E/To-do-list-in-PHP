807a175acb14c0586d1eb43244587093ec3f54fb
Safarov-E <elkhan.safarov10@yandex.com>
2021-01-04 19:27:16 +0500

    создание конструктора

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -1,35 +1,32 @@
 <?php
     class QueryBuilder {
+        public $pdo;
+        function __construct() {
+            $this->pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
+        }
         function getAllTasks() {
-            $pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
-            $statement = $pdo->prepare("SELECT * FROM  tasks");
+            $statement = $this->pdo->prepare("SELECT * FROM  tasks");
             $statement->execute();
             return $statement->fetchAll(2);
         }
         function addTask($data) {
-            $pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
-            $statement = $pdo->prepare("INSERT INTO tasks (title, content) VALUES (:title, :content)");
+            $statement = $this->pdo->prepare("INSERT INTO tasks (title, content) VALUES (:title, :content)");
             $statement->execute($data);
         }
         function getTask($id) {
-            $pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
-            $statement = $pdo->prepare("SELECT * FROM tasks WHERE id=:id");
+            $statement = $this->pdo->prepare("SELECT * FROM tasks WHERE id=:id");
             $statement->bindParam(":id", $id);
             $statement->execute();
             return $statement->fetch(PDO::FETCH_ASSOC);
         }
         function updateTask($data) {
-            $pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
-            $statement = $pdo->prepare("UPDATE tasks SET title=:title, content=:content WHERE id=:id");
+            $statement = $this->pdo->prepare("UPDATE tasks SET title=:title, content=:content WHERE id=:id");
             $statement->execute($data);
         }
         function deleteTask($id) {
-            $pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
-            $statement = $pdo->prepare("DELETE FROM tasks WHERE id=:id");
+            $statement = $this->pdo->prepare("DELETE FROM tasks WHERE id=:id");
             $statement->bindParam(":id", $id);
             $statement->execute();
-            
-            header("Location: /"); exit;
         }
     }
 ?>
